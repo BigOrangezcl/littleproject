7c593f86931b740178a3ac77894d4d8ff5b40f23
BigOrange <wx_5006c18c58104eeb8a084fa8dd15c80c@git.code.tencent.com>
2019-08-30 14:51:14 +0800

    little project

---
+++ pages/data/posts-data.js
@@ -0,0 +1,51 @@
+var local_database = [{
+    date: "Aug 5 2019",
+    title: "回到丽江",
+    imgSrc: "/images/pic8.jpg",
+    avatar: "/images/avatar/1.png",
+    content: "在即将离开的最后一天，没有去景点，因为住在古城，所以睡到自然醒，然后吃完早餐就在古城里面走了走，因为是下午的飞机，没把古城逛完就回去收拾行李了，云南的景点都很美，我还会回去的，回到丽江。",
+    reading: "112",
+    collection: "345",
+  },
+
+  {
+    date: "Aug 9 2019",
+    title: "孩子王",
+    imgSrc: "/images/pic7.jpg",
+    avatar: "/images/avatar/1.png",
+    content: "大屁孩带着两个小屁孩，好温馨的画面呢，希望自己童心未泯，留着以后和自己的孩子们也可以这样相处，哈哈哈，这对青梅竹马都喜欢粘着我，现在才发现我有吸小孩的魔力，要是当初去当幼师结局又会怎样呢？",
+    reading: "2390",
+    collection: "932",
+  },
+  {
+    title: "七彩云南有生之年一定要去一次",
+    content: "那边真的很美，山清水秀，人儿美丽，但是去到摩梭人的世界会觉得自己的生活真的来之不易，摩梭人居住的地方真的很穷，可能这就是需要发展旅游业的原因",
+    imgSrc: "/images/pic5.jpg",
+    reading: 540,
+    collection: 320,
+    date: "Aug 12 2019",
+    avatar: "/images/avatar/1.png"
+  },
+  {
+    title: "泸沽湖的水清人淳朴",
+    content: "泸沽湖的水真的特别的清，但是那边的阿哥好像都是靠划船为生，现在发展旅游业，阿哥们真的是靠力气干活",
+    imgSrc: "/images/pic4.jpg",
+    reading: 540,
+    collection: 320,
+    date: "Aug 12 2019",
+    avatar: "/images/avatar/1.png"
+  },
+  {
+    title: "七彩云南有生之年一定要去一次",
+    content: "那边真的很美，山清水秀，人儿美丽，但是去到摩梭人的世界会觉得自己的生活真的来之不易，摩梭人居住的地方真的很穷，可能这就是需要发展旅游业的原因",
+    imgSrc: "/images/pic5.jpg",
+    reading: 540,
+    collection: 320,
+    date: "Aug 12 2019",
+    avatar: "/images/avatar/1.png"
+  }
+]
+
+module.exports = {
+  postList:local_database
+}
